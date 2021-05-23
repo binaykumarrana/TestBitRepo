f04e7a53c45afb66298c4d4dd16645051ee8ea37
binaykumarrana <binaykumarrana@gmail.com>
2021-05-23 11:59:00 +0800

    Add navigation graph ui

---
+++ app/src/main/java/com/binay/testbitrepo/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.binay.testbitrepo
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Copyright 2021 Payed Pvt. Ltd.
+ *
+ * Created by Binay on 23/5/21.
+ */
+class HomeFragment: Fragment(R.layout.home_fragment) {
+}
