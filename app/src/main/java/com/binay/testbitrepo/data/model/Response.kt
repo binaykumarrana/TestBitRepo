8b7b5a7e11d1dc1b3e3e35eb11003dc293bda876
binaykumarrana <binaykumarrana@gmail.com>
2021-05-23 12:27:24 +0800

    Add repository model class

--- app/src/main/java/com/binay/testbitrepo/data/model/Response.kt
+++ app/src/main/java/com/binay/testbitrepo/data/model/Response.kt
@@ -1,9 +1,33 @@
 package com.binay.testbitrepo.data.model
 
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
 /**
 
  *
  * Created by Binay on 23/5/21.
  */
-class Response {
-}
+@Parcelize
+data class Repository(
+    @SerializedName("updated_on")
+    val updated_on: String = "",
+    @SerializedName("fork_policy")
+    val fork_policy: String = "",
+    @SerializedName("full_name")
+    val full_name: String = "",
+    @SerializedName("language")
+    val language: String = "",
+    @SerializedName("is_private")
+    val is_private: Boolean = false,
+    @SerializedName("description")
+    val description: String = "",
+) : Parcelable
+
+data class RepositoryList(
+    @SerializedName("values")
+    val cocktailList: List<Repository> = listOf(),
+    @SerializedName("next")
+    val next: String = ""
+)
